a2ffdbcac5359d5ff975bf03a429dc37066f39cd
pavanRMG <npavankumar2011@gmail.com>
2021-09-15 16:01:17 +0530

    first commit

--- FactorialOfANunmber.java
+++ FactorialOfANunmber.java
@@ -4,7 +4,7 @@ public class FactorialOfANunmber {//5!=5*4*3*2*1
 
 	public static void main(String[] args) {
 		int i,factriol=1;  
-		  int number=5;   //It is the number to calculate factorial    
+		  int number=5;   //It is the number to calculate factorial  hai  
 		  for(i=1;i<=number;i++){    
 		      factriol=factriol*i;    
 		  }    
